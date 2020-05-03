144c17206f134e372b1d0cedb30de63a14b29225
Luis R. de la Vega Lopez <luisr.delavega@gmail.com>
2020-05-03 15:30:54 -0400

    Main Menu changes

---
+++ Assets/Scripts/MainMenu/BackgroundScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundScroll : MonoBehaviour
+{
+  [SerializeField] private int xSeconds = 20;
+
+  void Update()
+  {
+    Material material = GetComponent<Image>().material;
+    Vector2 offset = material.mainTextureOffset;
+    offset.x += Time.deltaTime / xSeconds;
+
+    material.mainTextureOffset = offset;
+  }
+}
