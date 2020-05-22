fed8487923a45438261188168f0300c4efd56706
Luis R. de la Vega Lopez <luisr.delavega@gmail.com>
2020-05-22 18:23:46 -0400

    Added scene transitions.

---
+++ Assets/Scripts/MainMenu/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+  public Animator transition;
+
+  public void Load(string sceneName) => StartCoroutine(LoadLevel(sceneName));
+
+  public IEnumerator LoadLevel(string sceneName)
+  {
+    Debug.Log("Start!");
+    transition.SetTrigger("Start");
+    yield return new WaitForSeconds(1f);
+    SceneManager.LoadScene(sceneName);
+    Debug.Log("END!");
+  }
+}
